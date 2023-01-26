a8449e6ed2eeb75937e383cabd7710918cc4dc2b
w854155226 <42252108+w854155226@users.noreply.github.com>
2023-01-26 21:38:01 +0800

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,18 @@
+sudo apt-get install libpcap-dev libnids-dev libnet1-dev
+wget https://ftp.gnu.org/gnu/m4/m4-latest.tar.gz
+sudo tar -zxvf m4-1.4.19
+cd m4-1.4.19
+sudo ./configure
+sudo make
+sudo make install
+cd
+sudo apt-get install flex
+sudo apt-get install bison
+wget https://www.tcpdump.org/release/libpcap-1.10.3.tar.gz
+tar -zxvf libpcap-1.10.3.tar.gz
+cd libpcap-1.10.3
+sudo ./configure
+sudo make
+sudo make install
+find /usr -name "libpcap*so*"
+cd
